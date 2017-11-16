82aec3a56e7495fe2f1de5877dffc7308d3954a0
RitaJ <ritaj777@gmail.com>
2017-11-16 14:22:02 +0200

    Ištaisyta klaida. Pirmoje if sąlygoje "!= pakeista į >". Pakeisti pradiniai duomenys.

--- Uzduotys/5_1_Uzduotis.php
+++ Uzduotys/5_1_Uzduotis.php
@@ -2,14 +2,31 @@
 trikampio kraštinių ilgiai ir jei gali tai kokio trikampio: lygiakraščio, 
 lygiašonio ar įvairiakraščio. Atspausdinkite atsakymą. Kaip pradinius 
 duomenis panaudokite tokius skaičius: 
-3, 4, 5 
+1, 2, 4 
 2, 10, 8 
 5, 6, 5 
 5, 5, 5 
 -->
 
 <?php
-	$arrayData = array(array(3, 4, 5),array(2, 10, 8),array(5, 6, 5),array(5, 5, 5));
+	echo "<h1>Užduotis 5.1</h1>";
+    echo "<br>";
+    echo "Duoti trys skaičiai: a,b,c. Nustatykite ar šie skaičiai gali būti 
+	trikampio kraštinių ilgiai ir jei gali tai kokio trikampio: lygiakraščio, 
+	lygiašonio ar įvairiakraščio. Atspausdinkite atsakymą. Kaip pradinius 
+	duomenis panaudokite tokius skaičius:" ;
+	echo "<br>";
+	echo "1, 2, 4";
+	echo "<br>";
+	echo "2, 10, 8 ";
+	echo "<br>";
+	echo "5, 6, 5 ";
+	echo "<br>";
+	echo "5, 5, 5 ";
+    echo "<br>";
+    echo "<br>";
+
+	$arrayData = array(array(1, 2, 4),array(2, 10, 8),array(5, 6, 5),array(5, 5, 5));
 	$n = count($arrayData);					//skaičiuoja Masyvo $arrayData elementų skaičių
 
 	for ($i=0; $i<$n; $i++){
@@ -18,7 +35,7 @@ duomenis panaudokite tokius skaičius:
 		$b = $arrayData[$i][++$j];
 		$c = $arrayData[$i][++$j];
 
-		if ($a + $b != $c && $a + $c != $b && $b + $c != $a){
+		if ($a + $b > $c && $a + $c > $b && $b + $c > $a){
 			if ($a == $b && $b == $c){
 				echo "Šis trikampis yra lygiakraštis: " .$a ." " .$b ." ".$c;
 				echo '<br />';
