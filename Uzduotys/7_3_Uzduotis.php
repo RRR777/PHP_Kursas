1f43f121f7f4acf25f248d880f5894db2b49f4a5
RitaJ <ritaj777@gmail.com>
2017-11-27 11:59:06 +0200

    pritaikyta universaliam stulpeliu skaiciui

--- Uzduotys/7_3_Uzduotis.php
+++ Uzduotys/7_3_Uzduotis.php
@@ -1,12 +1,10 @@
-<!-- #7 - 3 Uždavinys - Turime daugiamatį masyvą, kuris aprašo kažką panašaus į 
+<?php  /* #7 - 3 Uždavinys - Turime daugiamatį masyvą, kuris aprašo kažką panašaus į 
 Excel lentelę arba matricą, t.y. pirmas indeksas žymi eilutę, o antras stulpelį. 
 Eilutės gali turėti skirtingą elementų (stulpelių) skaičių. Suskaičiuokite 
 stulpeliuose esančių skaičių sumas ir išveskite didžiausią 
 
-testuokite su masyvu: $a = [ [1, 3, 4], [2, 5], [2 => 3, 5=> 8], [1, 1, 5 => 1] ];
--->
+testuokite su masyvu: $a = [ [1, 3, 4], [2, 5], [2 => 3, 5=> 8], [1, 1, 5 => 1] ];  */
 
-<?php
     echo "<h1>Užduotis 7.3</h1> <br>";
 
     echo "Turime daugiamatį masyvą, kuris aprašo kažką panašaus į 
@@ -15,10 +13,21 @@ testuokite su masyvu: $a = [ [1, 3, 4], [2, 5], [2 => 3, 5=> 8], [1, 1, 5 => 1] 
          stulpeliuose esančių skaičių sumas ir išveskite didžiausią. <br> <br>";
     echo "<b>Pirmas variantas: </b><br>"; 
 
-    $a = [[1, 3, 4], [2, 5], [2=>3, 5=>8], [1, 1, 5=>1]];  
+    $a = [[1, 3, 4], [2, 5], [2=>3, 5=>8], [1, 1, 5=>2]];  
+    
     $max = 0;
-    $stulp = 6;
-    $st_suma = 0;
+    $st_suma = 0;   
+
+    foreach ($a as $key => $value){      // ($i=0; $i<$stulp; $i++){
+        foreach ($value as $key1 => $value1){  
+          $st_suma = sumavimas ($a, $key1);
+            if ( $st_suma > $max) {
+              $max = $st_suma;
+              $index = $key1;  
+            }   
+        }
+    }
+    echo "Maksimali stulpelio $index suma yra: $max <br><br><br><br>";  
 
     function sumavimas ($a, $st){
         $suma = 0;
@@ -29,37 +38,7 @@ testuokite su masyvu: $a = [ [1, 3, 4], [2, 5], [2 => 3, 5=> 8], [1, 1, 5 => 1] 
                 }
             } 
         }
-        echo $st ." stulpelio Suma yra: " .$suma  ." <br>";
         return $suma; 
     }
-
-    for ($i=0; $i<$stulp; $i++){
-        $st_suma = sumavimas ($a, $i);
-        if ( $st_suma > $max) {
-            $max = $st_suma;           
-        }
-    }     
-    echo "Maksimali stulpelio  suma yra: " .$max ."<br><br><br><br>";  
 ?>
 
-<?php
-    echo "<b>Antras variantas: </b><br><br>";
-
-    $a = [[1, 3, 4], [2, 5], [2=>3, 5=>8], [1, 1, 5=>1]];    
-    $max = 0;
-    $stulp = 6;  
-    
-    for ($i=0; $i< 6; $i++){
-        $st_suma = 0;
-        $column_numbers = array_column($a, $i);
-        $stulp = count ($column_numbers);
-            for ($j = 0; $j < $stulp; $j++){
-                 $st_suma += $column_numbers[$j];
-                 if ( $st_suma > $max) {
-                    $max = $st_suma;    
-            }     
-        }
-        echo "Stulpelio " .$i ." suma yra: " .$st_suma ."<br>"; 
-    }  
-        echo "Maksimali stulpelio  suma yra: " .$max ."<br>"; 
-?>
