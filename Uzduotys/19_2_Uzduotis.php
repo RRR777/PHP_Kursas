549363afa1744a7351aa57e0a74ad8be5678d85d
RitaJ <ritaj777@gmail.com>
2017-12-12 14:16:40 +0200

    Solved

---
+++ Uzduotys/19_2_Uzduotis.php
@@ -0,0 +1,13 @@
+<?php 
+// Parašykite užklausą, kuri išvestų vairuotojus, kurie vairavo automobilį 
+// bent dvi skirtingas dienas
+
+
+$sql = "SELECT COUNT(DISTINCT date) as `kiekis`, radars.driverId, name
+        FROM radars
+        LEFT OUTER JOIN drivers on radars.driverId = drivers.driverId
+        WHERE drivers.driverId is not null AND radars.driverId != 0 
+      	GROUP BY radars.driverId
+        HAVING kiekis >= 2
+        ORDER BY radars.driverId, name";
+?>
